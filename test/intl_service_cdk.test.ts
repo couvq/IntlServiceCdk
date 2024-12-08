1c98e2e3f58bfccd2843535f21c5c07fc3325cd8
couvq <quentincouvelaire@gmail.com>
2024-12-08 12:57:43 -0500

    initial sample cdk app

---
+++ test/intl_service_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IntlServiceCdk from '../lib/intl_service_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/intl_service_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IntlServiceCdk.IntlServiceCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
